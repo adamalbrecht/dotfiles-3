9d9422d1a41f7fd67b8dc4863b112d89e5bd6b56
Adam Albrecht <adam.albrecht@gmail.com>
2020-06-01 09:05:25 -0400

    Don't git pull prezto on rcup
    This caused issues. Instead, you're supposed to
    run `zprezto-update`

--- hooks/pre-up/fetch_prezto.sh
+++ hooks/pre-up/fetch_prezto.sh
@@ -2,6 +2,4 @@ if [ -d "${ZDOTDIR:-$HOME}/.zprezto" ]
 then
   cd "${ZDOTDIR:-$HOME}/.zprezto"
   git pull --quiet
-else
-  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" --quiet
 fi
